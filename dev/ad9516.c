b192720c99c20c807d0003af6149cd3de79ede66
Grzegorz Daniluk <grzegorz.daniluk@cern.ch>
2019-08-01 16:58:35 +0200

    ad9516: keep output 9 at 62.5MHz

--- dev/ad9516.c
+++ dev/ad9516.c
@@ -270,7 +270,6 @@ int ad9516_init(int scb_version)
 		ad9516_set_output_divider(spi_base, 3, 4, 0);  	// OUT3. 187.5 MHz. - not anymore
 
 		ad9516_set_output_divider(spi_base, 4, 1, 0);  	// OUT4. 500 MHz.
-		ad9516_set_output_divider(spi_base, 9, 20,0);
 
 		/*The following PLL outputs have been configured through the ad9516_base_config_34 register,
 		 * so it doesn't need to replicate the configuration:
