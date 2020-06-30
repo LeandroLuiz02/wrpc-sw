0cc3437c19fa71040a7da25723e8b1f3b7992ea7
Juan David Gonzalez Cobas <dcobas@cern.ch>
2020-06-30 12:24:05 +0200

    add some extension space for NCO status

--- libertm/libertm.h
+++ libertm/libertm.h
@@ -82,6 +82,7 @@ struct ertm_nco_reset {
 	uint32_t	current_stream_id;
 	uint32_t	rx_count;
 	uint32_t	reset_count;
+	uint32_t	unused[8];
 };
 
 /* as a general rule, all methods in libertm return an integer exit
