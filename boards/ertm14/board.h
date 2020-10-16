2210aae115b7a095d13f6aa9228d56b24850bc31
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2020-10-16 13:50:07 +0200

    boards/ertm15: adapt to Rev2 hardware. Rev1 support will be brought back as a separate BSP

--- boards/ertm14/board.h
+++ boards/ertm14/board.h
@@ -23,6 +23,11 @@
 #include "dev/iuart.h"
 #include "rf_frame_transceiver.h"
 
+#define BOARD_HAS_CUSTOM_NETWORK_INIT 1
+
+#undef BOARD_ERTM14_REV_1
+#define BOARD_ERTM14_REV_2
+
 #define BOARD_MAX_CONSOLE_DEVICES (2 + HAS_NETCONSOLE + HAS_PUTS_SYSLOG)
 
 #define BOARD_USE_EVENTS 1
@@ -169,6 +174,7 @@ struct ertm14_board
     struct fine_pulse_gen_device dds_sync_dev;
     struct iuart_device iuart_14;
     struct wr_rf_frame_transceiver_device rf_xcvr;
+    struct gpio_device gpio_ertm15_leds;
 
     int mode;
     int dds_resync_count;
