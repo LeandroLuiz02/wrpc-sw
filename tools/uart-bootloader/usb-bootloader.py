e50ff904c1274561227777fbe323913069dfbe9e
Juan David Gonzalez Cobas <dcobas@cern.ch>
2020-12-01 16:14:15 +0100

    use python3 by default in usb-bootloader

--- tools/uart-bootloader/usb-bootloader.py
+++ tools/uart-bootloader/usb-bootloader.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 #
 # DSI Shield
@@ -370,8 +370,9 @@ def run_terminal(ser):
         a = ser.recv_nonblock()
         if (a != None):
             sys.stderr.write(chr(a))
-            # flush is needed FIXME:
-            # sys.stderr.flush()
+            # sys.stderr.write(a).decode('utf-8'))
+	    # flush is needed FIXME:
+	    # sys.stderr.flush()
 
         a = os.read(sys.stdin.fileno(), 1)
         if a and ord(a) == 1:
@@ -412,7 +413,7 @@ def main(argv):
         elif opt in ("-b", "--board"):
             board_target = arg
         elif opt in ("-f", "--flash"):
-    	    flash_target = arg
+            flash_target = arg
             do_flash = True
         elif opt in ("-p", "--port"):
             our_port = arg
