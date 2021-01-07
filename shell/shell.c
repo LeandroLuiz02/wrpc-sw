3587f76db70a779f921a23e086304e5c3325f89a
Adam Wujek <adam.wujek@creotech.pl>
2021-01-07 14:34:16 +0100

    shell: bring back time cmd
    Takes ~400B
    
    Signed-off-by: Adam Wujek <adam.wujek@creotech.pl>

--- shell/shell.c
+++ shell/shell.c
@@ -367,6 +367,7 @@ void shell_register_commands(void)
 	REGISTER_WRC_COMMAND(stat);
 	REGISTER_WRC_COMMAND(ver);
 	REGISTER_WRC_COMMAND(ptrack);
+	REGISTER_WRC_COMMAND(time);
 	if (HAS_IP)
 		REGISTER_WRC_COMMAND(ip);
 	if (HAS_VLANS)
