b7607e4007197a7c312d382fe2ed6166918a93a0
Juan David Gonzalez Cobas <dcobas@cern.ch>
2021-02-19 12:02:01 +0100

    add rules to build sdbfs custom images

--- boards/boards.mk
+++ boards/boards.mk
@@ -8,6 +8,9 @@ obj-$(CONFIG_TARGET_ERTM14) += boards/ertm14/common-uart-link.o boards/ertm14/wr
 obj-$(CONFIG_TARGET_PXIE_FMC) += boards/pxie-fmc/board.o
 obj-$(CONFIG_TARGET_WR2RF_VME) += boards/wr2rf-vme/board.o  boards/wr2rf-vme/sdbfs-custom-image.o boards/ertm14/phy_calibration.o
 
+boards/ertm14/sdbfs-custom-image.o: boards/ertm14/sdbfs-custom-image.h
+boards/wr2rf-vme/sdbfs-custom-image.o: boards/wr2rf-vme/sdbfs-custom-image.h
+
 boards/ertm14/sdbfs-custom-image.h: boards/ertm14/sdbfs
 	./tools/gensdbfs -c boards/ertm14/sdbfs-custom-image.h boards/ertm14/sdbfs boards/ertm14/sdbfs-custom-image.bin
 
