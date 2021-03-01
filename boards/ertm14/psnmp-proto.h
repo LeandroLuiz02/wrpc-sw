5ca6fa5191f0d719ecdbb30e7057bbbd0b2993dc
Juan David Gonzalez Cobas <dcobas@cern.ch>
2021-03-01 17:52:46 +0100

    rename struct to singular

--- boards/ertm14/psnmp-proto.h
+++ boards/ertm14/psnmp-proto.h
@@ -15,7 +15,7 @@
 #define ertm14_get_sim_board_config	0x17
 #define	ertm14_comm_test		0x18
 
-struct ertm14_protocol_ops {
+struct ertm14_protocol_op {
 	int8_t	opcode;
 	size_t	offset1;
 	size_t	length1;
