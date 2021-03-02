1c45411069add4f87e86afc3dbaf7146466d8344
Juan David Gonzalez Cobas <dcobas@cern.ch>
2021-03-02 16:00:42 +0100

    usb-bootloader: add last updates of Tom's

--- tools/uart-bootloader/usb-bootloader.py
+++ tools/uart-bootloader/usb-bootloader.py
@@ -163,9 +163,11 @@ class DSIBootloader:
     RSP_BAD_CRC = 4
     RSP_HELLO = 5
 
-    def command(self, cmd, data):
+    def command(self, cmd, data, expect_response=True):
         while True:
             self.sock.tx_frame(cmd, data)
+            if( not expect_response ):
+                return 0
             status = self.sock.rx_frame()[0]
 
             if (status != self.RSP_CRC_ERROR):
@@ -205,10 +207,10 @@ class DSIBootloader:
 #	print(len(buf))
         return self.command(self.CMD_WRITE_RAM, buf)
 
-    def cmd_jump(self, addr):
+    def cmd_jump(self, addr, expect_response=True):
         buf = [(addr >> 24) & 0xff, (addr >> 16) & 0xff, (addr >> 8) & 0xff,
                addr & 0xff]
-        return self.command(self.CMD_GO, buf)
+        return self.command(self.CMD_GO, buf,expect_response=expect_response)
 
     def boot_enter(self):
         self.sock.reset_board()
@@ -251,7 +253,8 @@ class DSIBootloader:
         else:
             raise Exception("Unknown flash target: %s" % target)
         self.do_program_flash(image, offset, sector_size=0x400)
-        self.cmd_jump( 0x0 )
+        print("Flash programmed, launching the code...")
+        self.cmd_jump( 0x0,expect_response=False )
 
     def program_ertm14_wrc(self, fw, target):
         if target.lower() == "fpga":
@@ -282,7 +285,6 @@ class DSIBootloader:
 
     def do_program_flash(self, fw, offset = 0, sector_size = 0x10000):
         remaining = len(fw)
-        
         for i in range( offset // sector_size,
                        (offset + (remaining + sector_size - 1)) // sector_size):
             sys.stdout.write("\rErasing sector 0x%x          " %
@@ -310,6 +312,8 @@ class DSIBootloader:
         print(
             "\nFlashing complete."
         )
+
+        self.cmd_jump( 0x4 )
 
     def load_ram(self, image, addr):
         remaining = len(image)
@@ -373,9 +377,8 @@ def run_terminal(ser):
         a = ser.recv_nonblock()
         if (a != None):
             sys.stderr.write(chr(a))
-            # sys.stderr.write(a).decode('utf-8'))
-	    # flush is needed FIXME:
-	    # sys.stderr.flush()
+            if(chr(a) == '\n'):
+                sys.stderr.write('\r')
 
         a = os.read(sys.stdin.fileno(), 1)
         if a and ( ord(a) == 1 or ord(a) == 4 ):
