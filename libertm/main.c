97ef45356e504bb576a111a8f877360690c7417d
Juan David Gonzalez Cobas <dcobas@cern.ch>
2021-03-04 18:28:04 +0100

    add wrc diagnostics get op to libertm
    this is final (sort of)

--- libertm/main.c
+++ libertm/main.c
@@ -31,6 +31,8 @@ int main(int argc, char *argv[])
 	}
 	ertm_get_board_config(handle, &handle->state->board_state);
 	display_ertm_state(handle->state);
+    	ertm_wr_diags(handle, &handle->state->wr_status);
+	display_wrc_diags(&handle->state->wr_status);
 
 	ertm_exit(handle);
 
