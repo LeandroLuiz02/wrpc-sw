46d1e0c80f098d470052c5e2d3e2c68d2023db6c
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2021-03-17 15:30:27 +0100

    dev: include blinkenlights driver in the makefile

--- dev/dev.mk
+++ dev/dev.mk
@@ -29,7 +29,8 @@ obj-$(CONFIG_EMBEDDED_NODE) += \
 	dev/i2c_eeprom.o \
 	dev/storage.o \
 	dev/fine_pulse_generator.o \
-	dev/netif.o
+	dev/netif.o \
+	dev/leds.o
 
 obj-$(CONFIG_WR_NODE) += \
 	dev/sensors.o \
