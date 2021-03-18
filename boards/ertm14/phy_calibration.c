f2d95433bc5b33b7e7f1658a7accf63373cb4917
Juan David Gonzalez Cobas <dcobas@cern.ch>
2021-03-18 16:07:14 +0100

    fix pointer signedness (watch out!)

--- boards/ertm14/phy_calibration.c
+++ boards/ertm14/phy_calibration.c
@@ -137,7 +137,7 @@ static void tx_fsm_init(struct wrc_port_tx_setup_state *fsm)
 
     /* FIXME: is cal_saved_phase unsigned? uint32_t? declare it so
      * at the wrc_port_tx_setup_state structure */
-    if( !storage_get_calibration_parameter( CAL_PARAM_PHY_TARGET_TX_PHASE, &fsm->cal_saved_phase ) )
+    if( !storage_get_calibration_parameter( CAL_PARAM_PHY_TARGET_TX_PHASE, (uint32_t *)&fsm->cal_saved_phase ) )
     {
         phy_dbg("LPDC Tx target phase from calibration data: %d ps\n", fsm->cal_saved_phase);
         fsm->cal_saved_phase_valid = 1;
