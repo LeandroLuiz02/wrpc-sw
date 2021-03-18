9aa4c44d6224cbe8220a9f8ed74ac6a48a66d51f
Juan David Gonzalez Cobas <dcobas@cern.ch>
2021-03-18 16:19:56 +0100

    note unused variable

--- boards/ertm14/phy_calibration.c
+++ boards/ertm14/phy_calibration.c
@@ -314,7 +314,8 @@ static int tx_fsm_update(void)
 
     case TX_SETUP_DONE:
     {
-	ep_pcs_read(&wrc_endpoint_dev, MDIO_DBG0) & MDIO_DBG0_LINK_UP;
+	/* FIXME: this code seems to have no effect */
+	int early_link_up = ep_pcs_read(&wrc_endpoint_dev, MDIO_DBG0) & MDIO_DBG0_LINK_UP;
 
         return 1;
         break;
