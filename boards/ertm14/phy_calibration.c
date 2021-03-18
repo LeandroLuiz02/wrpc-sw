982b84d76c48b1f6612348606934b3d88e899aa3
Juan David Gonzalez Cobas <dcobas@cern.ch>
2021-03-18 16:09:26 +0100

    remove unused 'early_link_up' variable

--- boards/ertm14/phy_calibration.c
+++ boards/ertm14/phy_calibration.c
@@ -315,7 +315,7 @@ static int tx_fsm_update(void)
 
     case TX_SETUP_DONE:
     {
-    	int early_link_up = ep_pcs_read(&wrc_endpoint_dev, MDIO_DBG0) & MDIO_DBG0_LINK_UP;
+	ep_pcs_read(&wrc_endpoint_dev, MDIO_DBG0) & MDIO_DBG0_LINK_UP;
 
         return 1;
         break;
