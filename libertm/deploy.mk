e50f6a7a0320d8e405898c9456a5ee554caaf5d8
Juan David Gonzalez Cobas <dcobas@cern.ch>
2021-03-19 18:34:43 +0100

    add deployment logic

---
+++ libertm/deploy.mk
@@ -0,0 +1,12 @@
+VERSION = 1.0.0
+DEPLOY_TARGET ?= /acc/local/L867/drv/ertm/$(VERSION)
+TOOLS = ../tools/uart-bootloader/usb-bootloader.py ertm-cli
+
+deploy: $(LIBS) libertm.h $(TOOLS) ../wrc.bin ertm-setup
+	mkdir -p $(DEPLOY_TARGET)/lib $(DEPLOY_TARGET)/include \
+		$(DEPLOY_TARGET)/tools $(DEPLOY_TARGET)/bin
+	install -b ../wrc.bin $(LIBS) -C $(DEPLOY_TARGET)/lib
+	install -b libertm.h -C $(DEPLOY_TARGET)/include
+	install -b $(TOOLS) -C $(DEPLOY_TARGET)/tools
+	(cd $(DEPLOY_TARGET)/bin ; ln -sf ../tools/* .)
+	install -b ertm-setup -C $(DEPLOY_TARGET)/bin
