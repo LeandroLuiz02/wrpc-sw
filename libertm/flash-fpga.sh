67c427197f32cb6b3ae72429c4052f5c01e8a2d8
Juan David Gonzalez Cobas <dcobas@cern.ch>
2021-03-25 13:59:53 +0100

    sanitize update-fpga script

--- libertm/flash-fpga.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-/user/twlostow/apps/usb-bootloader.py -p /dev/ttyUSB2 -b default -s 921600 -f fpga /user/twlostow/bitstreams/ertm14/current/ertm14_top.bin
-
-#just to force a reboot
-/user/twlostow/apps/usb-bootloader.py -p /dev/ttyUSB1 -b ertm14m0 -s 115200 -t -f mcu /user/twlostow/bitstreams/ertm14/current/mmc14.bin
-
