4fdbaa7907a1f46b90a7a7600905ca774cac21e5
Juan David Gonzalez Cobas <dcobas@cern.ch>
2021-03-30 11:54:32 +0200

    deploy: use git version, it's safer

--- libertm/deploy.mk
+++ libertm/deploy.mk
@@ -1,4 +1,4 @@
-ERTM_VERSION ?= 1.0.0-rc1
+ERTM_VERSION ?= $(shell git describe --always --dirty | sed 's;libertm-v;;' )
 DEPLOY_TARGET ?= /acc/local/L867/drv/ertm/$(ERTM_VERSION)
 TOOLS = ../tools/uart-bootloader/usb-bootloader.py ertm-cli udev-find
 
