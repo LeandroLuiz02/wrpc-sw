1f4fa65b3c99fd70d1b383b99afd843aebab4bc2
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2021-04-12 15:22:26 +0200

    lib/snmp: temporarily disable temperature support in SNMP (needs to fixup the API after sensor rework)

--- lib/snmp.c
+++ lib/snmp.c
@@ -1221,6 +1221,7 @@ static int get_temp(uint8_t *buf, struct snmp_oid *obj)
 	row = obj->oid_match[TABLE_ROW];
 	col = obj->oid_match[TABLE_COL];
 	snmp_verbose("%s: row%d, col%d\n", __func__, row, col);
+	#if 0
 	for (p = wrc_temp_getnext(NULL); p; p = wrc_temp_getnext(p), i++) {
 		if (row == i) {
 			t = p->t;
@@ -1248,6 +1249,7 @@ static int get_temp(uint8_t *buf, struct snmp_oid *obj)
 		/* data found return it */
 		return get_value(buf, obj->asn, buffer);
 	}
+	#endif
 
 	return 0;
 }
