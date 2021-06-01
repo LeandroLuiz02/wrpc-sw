e5bef5d7f4b326d130ef0bda94214df50be6e590
Adam Wujek <adam.wujek@creotech.pl>
2021-06-01 04:14:24 +0200

    [Feature: #51] Kconfig: add subcommands to set advanced ptp atributes
    And add options to set default values in PPSI's Kconfig.
    
    Like:
    --prio1
    --prio2
    --domain (domain number)
    --class (clock class)
    --accuracy (clock accuracy)
    --allan (clock allan variance)
    --tsource (time source)
    
    This commit requires PPSI commit:
    arch-wrpc: add subcommands to set advanced ppsi parameters
    
    Signed-off-by: Adam Wujek <adam.wujek@creotech.pl>

--- shell/cmd_ptp.c
+++ shell/cmd_ptp.c
@@ -10,6 +10,20 @@
 #include "wrc_ptp.h"
 #include "shell.h"
 
+#ifdef CONFIG_CMD_PTP_ADV
+#define HAS_CMD_PTP_ADV 1
+#else
+#define HAS_CMD_PTP_ADV 0
+#endif
+
+#ifdef CONFIG_PTP_OPT_OVERWRITE_ATTRIBUTES
+#define HAS_PTP_OPT_OVERWRITE_ATTRIBUTES 1
+#else
+#define HAS_PTP_OPT_OVERWRITE_ATTRIBUTES 0
+#endif
+
+#define USE_CMD_PARAM -1
+
 struct subcmd {
 	char *name;
 	int (*fun)(int);
@@ -26,6 +40,18 @@ struct subcmd {
 	{"gm", wrc_ptp_set_mode, WRC_MODE_GM},
 	{"master", wrc_ptp_set_mode, WRC_MODE_MASTER},
 	{"slave", wrc_ptp_set_mode, WRC_MODE_SLAVE},
+#ifdef CONFIG_CMD_PTP_ADV
+	 /* use next param as a val for func */
+	{"prio1",    wrc_ptp_set_prio1, USE_CMD_PARAM},
+	{"prio2",    wrc_ptp_set_prio2, USE_CMD_PARAM},
+	{"domain",   wrc_ptp_set_domain_number, USE_CMD_PARAM},
+#ifdef CONFIG_PTP_OPT_OVERWRITE_ATTRIBUTES
+	{"class",    wrc_ptp_set_clock_class, USE_CMD_PARAM},
+	{"accuracy", wrc_ptp_set_clock_accuracy, USE_CMD_PARAM},
+	{"allan",    wrc_ptp_set_clock_allan_variance, USE_CMD_PARAM},
+	{"tsource",  wrc_ptp_set_time_source, USE_CMD_PARAM},
+#endif /* CONFIG_PTP_OPT_OVERWRITE_ATTRIBUTES */
+#endif /* CONFIG_CMD_PTP_ADV */
 #ifdef CONFIG_ABSCAL
 	{"abscal", wrc_ptp_set_mode, WRC_MODE_ABSCAL},
 #endif
@@ -44,6 +70,7 @@ static int cmd_ptp(const char *args[])
 {
 	int i, j, ret;
 	struct subcmd *c;
+	int l_arg;
 
 
 	if (!args[0]) {
@@ -51,13 +78,29 @@ static int cmd_ptp(const char *args[])
 			  is_run[wrc_ptp_run(-1)],
 			  is_mech[wrc_ptp_sync_mech(-1)],
 			  is_mode[wrc_ptp_get_mode()]);
+		if (HAS_CMD_PTP_ADV) {
+			wrc_ptp_set_prio1(USE_CMD_PARAM);
+			wrc_ptp_set_prio2(USE_CMD_PARAM);
+			wrc_ptp_set_domain_number(USE_CMD_PARAM);
+			if (HAS_PTP_OPT_OVERWRITE_ATTRIBUTES) {
+				wrc_ptp_set_clock_class(USE_CMD_PARAM);
+				wrc_ptp_set_clock_accuracy(USE_CMD_PARAM);
+				wrc_ptp_set_clock_allan_variance(USE_CMD_PARAM);
+				wrc_ptp_set_time_source(USE_CMD_PARAM);
+			}
+		}
 		return 0;
 	}
 
 	for (j = 0; args[j]; j++) {
 		for (i = 0, c = subcmd; i < ARRAY_SIZE(subcmd); i++, c++) {
 			if (!strcasecmp(args[j], c->name)) {
-				ret = c->fun(c->arg);
+				l_arg = c->arg;
+				/* if c->arg is USE_CMD_PARAM use next arg
+				 * as a parameter */
+				if (l_arg == USE_CMD_PARAM && args[++j])
+					l_arg = atoi(args[j]);
+				ret = c->fun(l_arg);
 				if (ret < 0)
 					return ret;
 				break;
