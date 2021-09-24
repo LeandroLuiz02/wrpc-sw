LeandroLuiz02/wrpc-sw

commit dece8203a6c1e983b2aea68bb3eb129bc01b512f

lib/latency: fix build
The code for latency was commented out

Signed-off-by: Adam Wujek <adam.wujek@creotech.pl>

shell/shell.c
@@ -63,6 +63,11 @@
 #define HAS_CMD_CONFIG 0
 #endif
 
+#ifdef CONFIG_LATENCY_PROBE
+#define HAS_LATENCY_PROBE 1
+#else
+#define HAS_LATENCY_PROBE 0
+#endif
 
 
 static char cmd_buf[SH_MAX_LINE_LEN + 1];
@@ -397,6 +402,8 @@ void shell_register_commands(void)
 	REGISTER_WRC_COMMAND(gui);
 	REGISTER_WRC_COMMAND(help);
 	REGISTER_WRC_COMMAND(init);
+	if (HAS_LATENCY_PROBE)
+		REGISTER_WRC_COMMAND(ltest);
 	if (HAS_IP)
 		REGISTER_WRC_COMMAND(ip);
 	if (HAS_CMD_LEAPSEC)

lib/latency.c
@@ -24,7 +24,6 @@ static int lat_verbose = 0;
 static int lat_verbose = 1;
 #endif
 
-#if 0
 static unsigned long prios[] = {7, 6, 0}; /* the prio for the 3 frames */
 
 static int ltest_fake_delay_ns;
@@ -306,5 +305,3 @@ DEFINE_WRC_COMMAND(ltest) = {
 	.name = "ltest",
 	.exec = cmd_ltest,
 };
-
-#endif

lib/latency.h
@@ -1,0 +1,12 @@
+/*
+ * This work is part of the White Rabbit project
+ *
+ * Released according to the GNU GPL, version 2 or any later version.
+ */
+#ifndef __LATENCY_H__
+#define __LATENCY_H__
+
+void latency_init(void);
+int latency_poll(void);
+
+#endif /* __LATENCY_H__ */

wrc_main.c
@@ -48,7 +48,7 @@
 #endif
 
 #ifdef CONFIG_LATENCY_PROBE
-//#include "lib/latency.h"
+#include "lib/latency.h"
 #endif
 
 #ifdef CONFIG_LLDP
@@ -248,11 +248,7 @@ static void create_tasks(void)
 #endif
 
 #ifdef CONFIG_LATENCY_PROBE
-#if 0
-	extern void latency_init(void);
-	extern void latency_poll(void);
 	wrc_task_create( "latency-probe", latency_init, latency_poll );
-#endif
 #endif
 
 #ifdef CONFIG_LLDP

commit 94556bda96bdec912b4f89fcace2ddd5efe6d7c9

shell: fix commands' order
fix wrong order introduced in commit:
dece8203 lib/latency: fix build

Signed-off-by: Adam Wujek <adam.wujek@creotech.pl>

shell/shell.c
@@ -427,12 +427,12 @@ void shell_register_commands(void)
 	REGISTER_WRC_COMMAND(gui);
 	REGISTER_WRC_COMMAND(help);
 	REGISTER_WRC_COMMAND(init);
-	if (HAS_LATENCY_PROBE)
-		REGISTER_WRC_COMMAND(ltest);
 	if (HAS_IP)
 		REGISTER_WRC_COMMAND(ip);
 	if (HAS_CMD_LEAPSEC)
 		REGISTER_WRC_COMMAND(leapsec);
+	if (HAS_LATENCY_PROBE)
+		REGISTER_WRC_COMMAND(ltest);
 	REGISTER_WRC_COMMAND(mac);
 	REGISTER_WRC_COMMAND(mode);
 	if (HAS_CMD_NETCONSOLE)