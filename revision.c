e79d1004486be228d2dd02f989c43ef875f8abb6
Adam Wujek <dev_public@wujek.eu>
2022-01-03 21:50:59 +0100

    revision: avoid buffer over-read
    Add as a last byte NULL char.
    
    Signed-off-by: Adam Wujek <dev_public@wujek.eu>

--- revision.c
+++ revision.c
@@ -27,8 +27,12 @@ struct spll_stats stats __attribute__((section(".stats"))) = {
 	.build_by = "",
 #else
 	.build_date = __DATE__,
+	.commit_id[sizeof(stats.build_date) - 1] = 0,
 	.build_time = __TIME__,
+	.commit_id[sizeof(stats.build_time) - 1] = 0,
 	.build_by = __GIT_USR__,
+	.commit_id[sizeof(stats.build_by) - 1] = 0,
 #endif
 	.commit_id = __GIT_VER__,
+	.commit_id[sizeof(stats.commit_id) - 1] = 0,
 };
