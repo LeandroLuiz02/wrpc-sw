51246f744c719a17fbaa0a40ecaace99c3526413
Adam Wujek <dev_public@wujek.eu>
2022-01-07 13:37:42 +0100

    lib/snmp: fix handling the end of sfp PN string
    Signed-off-by: Adam Wujek <dev_public@wujek.eu>

--- lib/snmp.c
+++ lib/snmp.c
@@ -307,6 +307,7 @@ static int get_time(uint8_t *buf, struct snmp_oid *obj);
 static int get_servo(uint8_t *buf, struct snmp_oid *obj);
 static int get_port(uint8_t *buf, struct snmp_oid *obj);
 static int get_temp(uint8_t *buf, struct snmp_oid *obj);
+static int get_sfp_pn_curr(uint8_t *buf, struct snmp_oid *obj);
 static int get_sfp(uint8_t *buf, struct snmp_oid *obj);
 static int get_mac(uint8_t *buf, struct snmp_oid *obj);
 static int get_aux_diag(uint8_t *buf, struct snmp_oid *obj);
@@ -440,7 +441,7 @@ static uint8_t oid_wrpcShellCmdReturnCode[] =    {4,0};
 	 OIDs */
 /* wrpcVersionGroup */
 static struct snmp_oid oid_array_wrpcVersionGroup[] = {
-	OID_FIELD_VAR(   oid_wrpcVersionHwType,      get_p,        NO_SET,   ASN_OCTET_STR, &wrc_hw_name),
+	OID_FIELD_VAR(   oid_wrpcVersionHwType,      get_pp,       NO_SET,   ASN_OCTET_STR, &wrc_hw_name),
 	OID_FIELD_VAR(   oid_wrpcVersionSwVersion,   get_pp,       NO_SET,   ASN_OCTET_STR, &build_revision),
 	OID_FIELD_VAR(   oid_wrpcVersionSwBuildBy,   get_pp,       NO_SET,   ASN_OCTET_STR, &build_by),
 	OID_FIELD_VAR(   oid_wrpcVersionSwBuildDate, get_pp,       NO_SET,   ASN_OCTET_STR, &snmp_build_date),
@@ -512,7 +513,7 @@ static struct snmp_oid oid_array_wrpcPtpConfigGroup[] = {
 /* wrpcPortGroup */
 static struct snmp_oid oid_array_wrpcPortGroup[] = {
 	OID_FIELD_VAR(   oid_wrpcPortLinkStatus,     get_port,     NO_SET,   ASN_INTEGER,   PORT_LINK_STATUS),
-	OID_FIELD_VAR(   oid_wrpcPortSfpPn,          get_p,        NO_SET,   ASN_OCTET_STR, &sfp_info.sfp_params.pn),
+	OID_FIELD_VAR(   oid_wrpcPortSfpPn,          get_sfp_pn_curr,NO_SET, ASN_OCTET_STR, NULL),
 	OID_FIELD_VAR(   oid_wrpcPortSfpInDB,        get_p,        NO_SET,   ASN_INTEGER,   &sfp_info.sfp_in_db),
 	OID_FIELD_VAR(   oid_wrpcPortInternalTX,     get_p,        NO_SET,   ASN_COUNTER,   &minic.tx_count),
 	OID_FIELD_VAR(   oid_wrpcPortInternalRX,     get_p,        NO_SET,   ASN_COUNTER,   &minic.rx_count),
@@ -1248,6 +1249,16 @@ static int get_temp(uint8_t *buf, struct snmp_oid *obj)
 	return 0;
 }
 
+/* sfp pn is without NULL termination */
+static int get_sfp_pn_curr(uint8_t *buf, struct snmp_oid *obj)
+{
+	char sfp_pn[SFP_PN_LEN + 1];
+
+	memcpy(sfp_pn, sfp_info.sfp_params.pn, SFP_PN_LEN);
+	sfp_pn[SFP_PN_LEN] = 0;
+
+	return get_value(buf, obj->asn, &sfp_pn);
+}
 
 static int get_sfp(uint8_t *buf, struct snmp_oid *obj)
 {
