8a2c8728f4e4677b12bb739b0b8b7afff89ba349
Juan David Gonzalez Cobas <dcobas@cern.ch>
2022-01-25 16:48:06 +0100

    add wrc_diags_regs.h exports and deploy

--- libertm/deploy.mk
+++ libertm/deploy.mk
@@ -3,11 +3,12 @@ DEPLOY_TARGET ?= /acc/local/L867/drv/ertm/$(ERTM_VERSION)
 TOOLS = ../tools/uart-bootloader/usb-bootloader.py ertm-cli udev-find
 
 deploy: $(LIBS) libertm.h $(TOOLS) ../wrc.bin ertm-setup
-	mkdir -p $(DEPLOY_TARGET)/lib $(DEPLOY_TARGET)/include \
+	mkdir -p $(DEPLOY_TARGET)/lib $(DEPLOY_TARGET)/include/hw \
 		$(DEPLOY_TARGET)/tools $(DEPLOY_TARGET)/bin
 	ln -s ../pyenv $(DEPLOY_TARGET)
 	install -b ../wrc.bin $(LIBS) -C $(DEPLOY_TARGET)/lib
-	install -b libertm.h -C $(DEPLOY_TARGET)/include
+	install -b libertm.h ../boards/ertm14/ertm-common.h -C $(DEPLOY_TARGET)/include
+	install -b ../include/hw/wrc_diags_regs.h -C $(DEPLOY_TARGET)/include/hw
 	install -b $(TOOLS) -C $(DEPLOY_TARGET)/tools
 	(cd $(DEPLOY_TARGET)/bin ; ln -sf ../tools/* .)
 	install -b ertm-setup -C $(DEPLOY_TARGET)/bin
