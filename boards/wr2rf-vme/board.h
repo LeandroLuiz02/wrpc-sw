9ac0e0c33534cc5640a91e396bda4d350613bb40
Tristan Gingold <tristan.gingold@cern.ch>
2023-02-03 15:51:09 +0100

    wr2rf-vme: switch to risc-v

--- boards/wr2rf-vme/board.h
+++ boards/wr2rf-vme/board.h
@@ -61,14 +61,23 @@ int board_update(void);
 
 #define SDBFS_REC 5
 
-#define BASE_AUXWB                  0x48000
-#define BASE_SOFTPLL                0x40200
-#define BASE_PPS_GEN                0x40300
-#define BASE_UART                   0x40500
-#define BASE_SYSCON                 0x40400
-#define BASE_EP                     0x40100
-#define BASE_MINIC                  0x40000
-#define BASE_ONEWIRE                0x40600
+#ifdef CONFIG_ARCH_RISCV
+#define DEV_BASE	0x100000
+#elif defined CONFIG_ARCH_LM32
+#define DEV_BASE	0x40000
+#else
+#error (Wrong Arch!)
+#endif
 
+/* Fixed base addresses */
+#define BASE_MINIC		(DEV_BASE + 0x000)
+#define BASE_EP			(DEV_BASE + 0x100)
+#define BASE_SOFTPLL		(DEV_BASE + 0x200)
+#define BASE_PPS_GEN 		(DEV_BASE + 0x300)
+#define BASE_SYSCON		(DEV_BASE + 0x400)
+#define BASE_UART		(DEV_BASE + 0x500)
+#define BASE_ONEWIRE		(DEV_BASE + 0x600)
+#define BASE_WDIAGS_PRIV       	(DEV_BASE + 0x900)
+#define BASE_AUXWB		(DEV_BASE + 0x8000)
 
 #endif /* __BOARD_WRC_H */
