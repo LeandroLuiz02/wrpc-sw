735fb3be3d03c531a50ca3de0a7382e40a05b7d4
Tristan Gingold <tristan.gingold@cern.ch>
2023-02-22 16:44:16 +0100

    cmd_ptrack.c: add help

--- shell/cmd_ptrack.c
+++ shell/cmd_ptrack.c
@@ -22,12 +22,16 @@ extern int wrc_phase_tracking;
 
 static int cmd_ptrack(const char *args[])
 {
-	if (args[0] && !strcasecmp(args[0], "enable")) {
+	if (!args[0]) {
+		pp_printf("ptrack enable | ps-freeze [phase] | vco-freeze\n");
+		return 0;
+	}
+	if (!strcmp(args[0], "enable")) {
 		pp_printf("UnFreezing SPLL phase shifter\n");
 		spll_vco_freeze(0);
 		spll_pshifter_freeze(0);
 	}
-	else if (args[0] && !strcasecmp(args[0], "ps-freeze")) {
+	else if (!strcmp(args[0], "ps-freeze")) {
 		if( args[1] )
 		{
 			pp_printf("Freezing SPLL phase shifter at phase %d ps\n", atoi(args[1]));
@@ -41,11 +45,13 @@ static int cmd_ptrack(const char *args[])
 		}
 
 	}
-	else if (args[0] && !strcasecmp(args[0], "vco-freeze"))
+	else if (!strcmp(args[0], "vco-freeze"))
 	{
 		pp_printf("Freezing SPLL VCO control");
 		spll_vco_freeze(1);
 	}
+	else
+		return -1;
 
 	return 0;
 }
