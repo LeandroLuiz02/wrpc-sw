6527b4652de696871f5f40a6052f45f7c6ff7281
Tristan Gingold <tristan.gingold@cern.ch>
2023-03-14 17:12:49 +0100

    pps_gen.c: remove a weak attribute

--- dev/pps_gen.c
+++ dev/pps_gen.c
@@ -96,8 +96,7 @@ static uint64_t pps_get_utc(void)
 	return out;
 }
 
-void __attribute__((weak)) shw_pps_gen_get_time(uint64_t * seconds,
-						  uint32_t * nanoseconds)
+void shw_pps_gen_get_time(uint64_t * seconds, uint32_t * nanoseconds)
 {
 	uint32_t ns_cnt;
 	uint64_t sec1, sec2;
