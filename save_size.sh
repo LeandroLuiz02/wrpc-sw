b42976adeb57530b1b1b7c00b3403084a01ca21f
Tristan Gingold <tristan.gingold@cern.ch>
2023-03-16 14:52:57 +0100

    save_size.sh: use a wrc.size by default

--- save_size.sh
+++ save_size.sh
@@ -3,7 +3,7 @@
 set -e
 
 if ! [ -n "$size_info_file" ]; then
-    exit 0
+    size_info_file="wrc.size"
 fi
 
 # separate calling commands and filling DB
@@ -13,3 +13,5 @@ GIT_HASH=`git log --format=format:%H -1`
 echo -n "$GIT_HASH " >> "$size_info_file"
 echo -n "$DEFCONFIG_NAME ">> "$size_info_file"
 echo $SIZES >> "$size_info_file"
+
+tail $size_info_file
