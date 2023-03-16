1dc0fde00bac153ad8efd7d40012651b39ba11bc
Tristan Gingold <tristan.gingold@cern.ch>
2023-03-16 16:27:24 +0100

    cmd_calib.c: use sub_cmd

--- shell/cmd_calib.c
+++ shell/cmd_calib.c
@@ -20,63 +20,25 @@
 #include "dev/syscon.h"
 #include "dev/rxts_calibrator.h"
 
+static const char * const calib_cmds[] =
+{
+	 [0] = "force",
+	 [1] = "load",
+	 [2] = "setp",
+#ifdef CONFIG_CMD_CALIBRATION_SHOW
+	 [3] = "show",
+#endif
+};
+
 static int cmd_calibration(const char *args[])
 {
 	uint32_t trans;
+	int icmd;
 
-	if (args[0])
-	{
-		if(!strcasecmp(args[0], "force")) 
-		{
-			if (measure_t24p(&trans) < 0)
-				return -1;
-			return storage_phtrans(&trans, 1);
-		}
-		else if (!strcasecmp( args[0], "load" ) )
-		{
-			storage_load_calibration();
-		}
-		else if (!strcasecmp( args[0], "setp" ) )
-		{
-			uint32_t param = 0;
-			param |= ((uint32_t)(args[1][0])) << 24;
-			param |= ((uint32_t)(args[1][1])) << 16;
-			param |= ((uint32_t)(args[1][2])) << 8;
-			param |= ((uint32_t)(args[1][3])) << 0;
-
-			int value = atoi(args[2]);
-
-			pp_printf("Setting calibration parameter %s [0x%x] to %d\n",
-				  args[1], (unsigned int) param, value);
-			storage_set_calibration_parameter( param, value );
-		}
-#ifdef CONFIG_CMD_CALIBRATION_SHOW
-		else if (!strcmp(args[0], "show")) {
-			wrc_cal_data_t *cal;
-			unsigned i;
-
-			if (!storage_is_calibration_loaded()) {
-				pp_printf("calibrations not loaded\n");
-				return 0;
-			}
-			cal = storage_get_calibration_data();
-			pp_printf("%u params:\n", (unsigned)cal->param_count);
-			for(i = 0; i < cal->param_count; i++) {
-				unsigned id = cal->params[i].id;
-				pp_printf( " %c%c%c%c = %u\n",
-					   (id >> 24) & 0xff,
-					   (id >> 16) & 0xff,
-					   (id >> 8) & 0xff,
-					   (id >> 0) & 0xff,
-					   (unsigned)cal->params[i].value);
-			}
-		}
-#endif
-	} else if (!args[0]) {
+	if (!args[0]) {
 		if (storage_phtrans(&trans, 0) > 0) {
 			pp_printf("Found phase transition in EEPROM: %dps\n",
 				  (unsigned int) trans);
-
 			return 0;
 		} else {
 			pp_printf("Measuring t2/t4 phase transition...\n");
@@ -87,7 +49,58 @@ static int cmd_calibration(const char *args[])
 		}
 	}
 
-	return 0;
+	icmd = sub_cmd(calib_cmds, ARRAY_SIZE(calib_cmds), args);
+
+	switch (icmd) {
+	case 0:
+		if (measure_t24p(&trans) < 0)
+			return -1;
+		return storage_phtrans(&trans, 1);
+	case 1:
+		storage_load_calibration();
+		return 0;
+	case 2:
+	{
+		uint32_t param = 0;
+		param |= ((uint32_t)(args[1][0])) << 24;
+		param |= ((uint32_t)(args[1][1])) << 16;
+		param |= ((uint32_t)(args[1][2])) << 8;
+		param |= ((uint32_t)(args[1][3])) << 0;
+
+		int value = atoi(args[2]);
+
+		pp_printf("Setting calibration parameter %s [0x%x] to %d\n",
+			  args[1], (unsigned int)param, value);
+		storage_set_calibration_parameter(param, value);
+		return 0;
+	}
+#ifdef CONFIG_CMD_CALIBRATION_SHOW
+	case 3:
+	{
+		wrc_cal_data_t *cal;
+		unsigned i;
+
+		if (!storage_is_calibration_loaded()) {
+			pp_printf("calibrations not loaded\n");
+			return 0;
+		}
+		cal = storage_get_calibration_data();
+		pp_printf("%u params:\n", (unsigned)cal->param_count);
+		for(i = 0; i < cal->param_count; i++) {
+			unsigned id = cal->params[i].id;
+			pp_printf( " %c%c%c%c = %u\n",
+				   (id >> 24) & 0xff,
+				   (id >> 16) & 0xff,
+				   (id >> 8) & 0xff,
+				   (id >> 0) & 0xff,
+				   (unsigned)cal->params[i].value);
+		}
+		return 0;
+	}
+#endif
+	default:
+		return icmd;
+	}
 }
 
 DEFINE_WRC_COMMAND(calibration) = {
