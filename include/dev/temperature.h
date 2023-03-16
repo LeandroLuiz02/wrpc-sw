46a15671bbcd7769b1a1f618aaf0875b1a4805f4
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2023-03-16 16:27:27 +0100

    dev: fix post-merge errors in sensors code

--- include/dev/temperature.h
+++ include/dev/temperature.h
@@ -19,45 +19,18 @@
 #define WRC_MAX_TEMPERATURES 0
 #endif
 
-<<<<<<<< HEAD:include/dev/temperature.h
 #define TEMP_INVALID (0x8000 << 16)
 
 /* A single temperature sensor */
 struct wrc_temp_sensor {
-========
-#define WRC_SENSOR_TEMP_CELSIUS (1<<0)
-#define WRC_SENSOR_CURRENT_MA (1<<1)
-#define WRC_SENSOR_VOLTAGE_MV (1<<2)
-#define WRC_SENSOR_VALID (1<<3)
-
-#define WRC_SENSOR_INVALID_VALUE (0x80000000)
-
-struct wrc_sensor
-{
-	const char* name;
-	uint8_t flags;
-	uint8_t id;
-	int16_t value;
-};
-
-struct wrc_onetemp {
->>>>>>>> tom-rf-ertm14-rebase:include/sensors.h
 	char *name;
 	int32_t t;  /* fixed point, 16.16 (signed!) */
 };
 
-<<<<<<<< HEAD:include/dev/temperature.h
 /* A list of temperature sensors */
 struct wrc_temp_group {
 	int (*read)(struct wrc_temp_sensor *);
 	struct wrc_temp_sensor *t; /* zero-terminated */
-========
-struct wrc_temp {
-	int used;
-	int (*read)(struct wrc_temp *);
-	void *data;
-	struct wrc_onetemp *t; /* zero-terminated */
->>>>>>>> tom-rf-ertm14-rebase:include/sensors.h
 };
 
 /* lib functions  */
@@ -72,11 +45,5 @@ int wrc_temp_register(struct wrc_temp_group *new_temp_sensor);
 #ifdef CONFIG_TEMP_SENSORS
 extern struct wrc_temp_group temp_sensors[WRC_MAX_TEMPERATURES];
 #endif
-
-/* generic sensor functions */
-void wrc_register_sensors( struct wrc_sensor* s);
-struct wrc_sensor* wrc_sensor_find_by_name(char *name);
-struct wrc_sensor* wrc_sensor_find_by_id(uint8_t id);
-struct wrc_sensor* wrc_sensor_find_by_type(uint8_t type);
 
 #endif /* __TEMPERATURE_H__ */
