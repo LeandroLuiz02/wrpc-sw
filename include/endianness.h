0f1b4a238424eb397b731eb0eac02401d408482b
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2023-03-30 16:38:20 +0200

    include: various fixes in endianess.h: - provide [bl]e[16|32]_to_host and _from_host inline functions - use compiler's __BYTE_ORDER__ macro to detect platform's endianess instead of WRPC's Kconfig macros - provide a htonll()/ntohll() implementation for ppsi

--- include/endianness.h
+++ include/endianness.h
@@ -13,38 +13,30 @@
 
 #include <stdint.h>
 
-# if defined CONFIG_ARCH_RISCV
-#  define __ENDIANNESS_SWAP 1
-# elif defined CONFIG_ARCH_LM32
-#  define __ENDIANNESS_SWAP 0
-# else
-#  error (Wrong Arch!)
-# endif
-
 /* Declare those functions as inline (and not as macro) so that they have
    an address (but only once).  */
 
 #define ntohl  htonl
 #define ntohs  htons
+#define ntohll  htonll
 
-#ifndef __PPSI_LIB_H__
-/* ppsi/lib.h also declares htonll.  */
-
-#define ntohll htonll
+#define le16_to_host host_to_le16
+#define le32_to_host host_to_le32
+#define be16_to_host host_to_be16
+#define be32_to_host host_to_be32
 
 static inline uint64_t htonll(uint64_t hostllong)
 {
-#if __ENDIANNESS_SWAP
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
 	return __builtin_bswap64(hostllong);
 #else
 	return hostllong;
 #endif
 }
-#endif
 
 static inline uint32_t htonl(uint32_t hostlong)
 {
-#if __ENDIANNESS_SWAP
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
 	return __builtin_bswap32(hostlong);
 #else
 	return hostlong;
@@ -53,7 +45,7 @@ static inline uint32_t htonl(uint32_t hostlong)
 
 static inline uint16_t htons(uint16_t hostshort)
 {
-#if __ENDIANNESS_SWAP
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
 	return __builtin_bswap16(hostshort);
 #else
 	return hostshort;
@@ -65,12 +57,48 @@ static inline uint16_t htons(uint16_t hostshort)
 /* Change endianess on a memory region */
 static inline void ntohl_mem(uint8_t *mem, int size_bytes)
 {
-#if __ENDIANNESS_SWAP
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
 	int i;
 
 	for (i = 0; i < size_bytes ; i += sizeof(uint32_t)) {
 		*(uint32_t*)(mem + i) = ntohl(*(uint32_t*)(mem + i));
 	}
+#endif
+}
+
+static inline uint32_t be32_to_host(uint32_t hostlong)
+{
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+	return __builtin_bswap32(hostlong);
+#else
+	return hostlong;
+#endif
+}
+
+static inline uint32_t le32_to_host(uint32_t hostlong)
+{
+#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+	return __builtin_bswap32(hostlong);
+#else
+	return hostlong;
+#endif
+}
+
+static inline uint16_t be16_to_host(uint16_t hostshort)
+{
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+	return __builtin_bswap16(hostshort);
+#else
+	return hostshort;
+#endif
+}
+
+static inline uint16_t le16_to_host(uint16_t hostshort)
+{
+#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+	return __builtin_bswap32(hostshort);
+#else
+	return hostshort;
 #endif
 }
 
