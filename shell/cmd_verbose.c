a26240060c49b44508e89c7bedfcba9542521af8
Tristan Gingold <tristan.gingold@cern.ch>
2023-04-06 16:21:50 +0200

    cmd_verbose: add meaning of each digits

--- shell/cmd_verbose.c
+++ shell/cmd_verbose.c
@@ -8,11 +8,29 @@
 #include <shell.h>
 #include <ppsi/ppsi.h>
 
+#if CONFIG_VERBOSE_DETAIL
+static const char diag_names[8][8] = {
+  "fsm    ",
+  "time   ",
+  "frames ",
+  "servo  ",
+  "bmc    ",
+  "ext    ",
+  "config ",
+  "---    "
+};
+#endif
+
 static int cmd_verbose(const char *args[])
 {
 	if (args[0])
 		pp_global_d_flags = pp_diag_parse(args[0]);
 	pp_printf("PPSI verbosity: %08lx\n", pp_global_d_flags);
+#if CONFIG_VERBOSE_DETAIL
+	for (unsigned i = 0; i < 8; i++)
+		pp_printf("%s %x\n", diag_names[i],
+			  (unsigned)(pp_global_d_flags >> ((7 - i) * 4)) & 0x0f);
+#endif
 	return 0;
 }
 
