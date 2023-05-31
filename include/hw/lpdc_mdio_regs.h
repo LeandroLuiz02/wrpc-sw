879d938d7aeb55dee2a3e28dec540cf84af32f66
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2023-05-31 15:06:46 +0200

    lpdc_mdio_regs: more LPDC-specific MDIO registers for the Kintex7 20ps fix

--- include/hw/lpdc_mdio_regs.h
+++ include/hw/lpdc_mdio_regs.h
@@ -25,6 +25,23 @@
 #define LPDC_MDIO_STAT_RX_RST_DONE 0x20UL
 #define LPDC_MDIO_STAT_COMMA_CURRENT_POS_MASK 0x7f80UL
 #define LPDC_MDIO_STAT_COMMA_CURRENT_POS_SHIFT 7
+#define LPDC_MDIO_STAT_COMMA_POS_VALID 0x8000UL
+
+/* Low Phase Drift Calibration Control Register 2 */
+#define LPDC_MDIO_CTRL2 0x8UL
+#define LPDC_MDIO_CTRL2_RX_RATE_MASK 0x7UL
+#define LPDC_MDIO_CTRL2_RX_RATE_SHIFT 0
+#define LPDC_MDIO_CTRL2_RX_LATCH_PATTERN 0x8UL
+#define LPDC_MDIO_CTRL2_RX_GEARBOX_PLL_RESET 0x10UL
+#define LPDC_MDIO_CTRL2_RX_GEARBOX_PLL_LOCKED 0x20UL
+#define LPDC_MDIO_CTRL2_RX_CDR_LOCKED 0x40UL
+
+/* None */
+#define LPDC_MDIO_IDLE_PAT 0x40UL
+#define LPDC_MDIO_IDLE_PAT_SIZE 4 /* 0x4 */
+
+/* None */
+#define LPDC_MDIO_IDLE_PAT_DATA 0x0UL
 
 /* Xilinx DRP registers, specific to the transceiver */
 #define LPDC_MDIO_DRP_REGS 0x1000UL
@@ -38,8 +55,23 @@ struct lpdc_mdio {
   /* [0x4]: REG (ro) Low Phase Drift Calibration Status Register */
   uint32_t STAT;
 
+  /* [0x8]: REG (rw) Low Phase Drift Calibration Control Register 2 */
+  uint32_t CTRL2;
+
+  /* padding to: 16 words */
+  uint32_t __padding_0[13];
+
+  /* [0x40]: REPEAT (no description) */
+  struct idle_pat {
+    /* [0x0]: REG (ro) (no description) */
+    uint32_t data;
+  } idle_pat[12];
+
+  /* padding to: 16 words */
+  uint32_t __padding_1[4];
+
   /* padding to: 1024 words */
-  uint32_t __padding_0[1022];
+  uint32_t __padding_2[992];
 
   /* [0x1000]: SUBMAP Xilinx DRP registers, specific to the transceiver */
   uint32_t drp_regs[1024];
