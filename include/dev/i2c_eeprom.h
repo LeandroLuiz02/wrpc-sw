c3fc3e0737a66ebad7b1a258591a9ababa3ca086
Frederik Pfautsch <frederik.pfautsch@missinglinkelectronics.com>
2024-06-05 16:48:58 +0200

    dev/i2c_eeprom.c: Use offset -2 when addr>0xff is mangled with dev addr
    On some I2C eeproms which require addresses >1B, the remaining bits are mangled
    together with the i2c device address. Thus the "magic offset_bytes" of -2 is
    introduced to cover these cases.
    
    Signed-off-by: Frederik Pfautsch <frederik.pfautsch@missinglinkelectronics.com>

--- include/dev/i2c_eeprom.h
+++ include/dev/i2c_eeprom.h
@@ -12,6 +12,37 @@
 
 #include "board.h"
 
+/*
+ * Some EEPROMs use the I2C single byte address
+ * scheme, but provide more memory than addressable
+ * by this single byte address. To make the
+ * additional memory space available virtual I2C
+ * devices are mapped onto the memory space similar
+ * to paging techniques.
+ *
+ * The EEPROM device m24c08 on the ZCU102 platform
+ * is one example. It maps the full address space
+ * spanning 10 bits to 4 virtual devices (2 address
+ * bits) with an address space of 8 bits each.
+ * To support this addressing scheme the meaning of
+ * the dev.offset_bytes field is modified:
+ * dev.offset_bytes = 2: payload data is place at
+ * Byte 2 ff in the I2C data stream and 2 address
+ * bytes are used.
+ * dev.offset_bytes = !2: payload data is placed
+ * at Byte 1 ff in the I2C data stream and single
+ * byte address is used
+ * dev.offset_bytes = -<bw>: a negative value
+ * indicates single byte addressing and the
+ * abs(dev.offset_bytes) denotes the number of Bits
+ * used of the address exceeding the single byte
+ * address, being mapped onto the device address to
+ * select the virtual devices.
+ *
+ * This adjustment needs to be done for every byte
+ * access to the device as one multi-byte access can
+ * span multiple virtual devices.
+ */
 struct i2c_eeprom_device {
     struct i2c_bus *bus;
     uint8_t addr;
